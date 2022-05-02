c65d2e660e281f35ad7c60f2432dbfb1f4d17548
Colm1711 <cosullivan1711@gmail.com>
2022-05-02 10:13:26 +0000

    Adjusted validation to ensure data entered are integars

--- run.py
+++ run.py
@@ -33,10 +33,12 @@ def validate_data(values):
     of if there arent't exactly 6 figures
     """
     try:
+        [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
-                f"Exactly 6 values are required, you entered {len(values)}."
+                f"Exactly 6 values are required, you entered {len(values)}"
             )
     except ValueError as e:
         print(f"Invalid data {e}, please try again. \n")
+
 get_sales_data()
